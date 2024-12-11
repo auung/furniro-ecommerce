ac8d781cdbe4ae3f9ad349ee5c2956eec8fdaf82
auung <akkyaw798@gmail.com>
2024-12-11 16:48:46 +0700

    chore: add new font weights

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,9 +1,9 @@
 import { Poppins, Montserrat, Lato } from "next/font/google";
 
 export const poppins = Poppins({
-	weight: "400",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const montserrat = Montserrat();
 
-export const lato = Lato({ weight: "400" });
+export const lato = Lato({ weight: ["100", "300", "400", "700", "900"] });
