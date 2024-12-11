352ea9e0535faab229ad87623384efc02ae22a88
auung <akkyaw798@gmail.com>
2024-12-11 16:58:27 +0700

    fix: add font subsets

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,8 +2,12 @@ import { Poppins, Montserrat, Lato } from "next/font/google";
 
 export const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
 });
 
-export const montserrat = Montserrat();
+export const montserrat = Montserrat({ subsets: ["latin"] });
 
-export const lato = Lato({ weight: ["100", "300", "400", "700", "900"] });
+export const lato = Lato({
+  weight: ["100", "300", "400", "700", "900"],
+  subsets: ["latin"],
+});
